d167475dca3b68e57a680ba7b07b558451668c13
Karol Kucinski <kucinski.k@e80group.com>
2023-06-11 03:03:51 +0200

    Add project files.

---
+++ Sharing/Sharing/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Sharing.Services;
+using Sharing.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sharing
+{
+    public partial class App : Application
+    {
+
+        public App() 
+        {
+            InitializeComponent();
+
+
+            DependencyService.Register<ILoginService,loginService>();
+            MainPage = new NavigationPage(new LoginPage2()); 
+            
+            //  DependencyService.Register<MockDataStore>();
+           // MainPage = new AppShell(); 
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Sharing/Sharing/Services/loginService.cs
@@ -0,0 +1,31 @@
+﻿using Sharing.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sharing.Services
+{
+    public class loginService : ILoginService
+    {
+        List<UserModel> userList = new List<UserModel>();
+
+
+        public loginService()
+        {
+            userList.Add(new UserModel { userName = "user1", password = "123456" }) ;  
+            userList.Add(new UserModel { userName = "user2", password = "1234567" }) ;  
+        
+        }
+        public bool login(string username, string password)
+        {
+            foreach(var user in userList) 
+            {
+                if(username==user.userName & password==user.password)
+                {
+                    return true;    
+                }
+            }
+            return false;
+        }
+    }
+}
